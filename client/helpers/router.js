e0bd065eebf1d63d0aa74676fd9bc77c40afdbc4
Ryan Allen <ryan@ryangallen.com>
2013-07-10 01:27:51 -0400

    denied access to new posts when not logged in

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -6,3 +6,12 @@ Meteor.Router.add({
 	},
 	'/submit': 'postSubmit'
 });
+Meteor.Router.filters({
+	'requireLogin': function(page){
+		if (Meteor.user())
+			return page;
+		else
+			return 'accessDenied';
+	}
+});
+Meteor.Router.filter('requireLogin', {only: 'postSubmit'});
